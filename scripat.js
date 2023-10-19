7277b940e328e5cae8979576534a0ab5c47e15ce
neeraj9927 <130194766+neeraj9927@users.noreply.github.com>
2023-10-19 20:42:03 +0530

    update style sheet

--- scripat.js
+++ scripat.js
@@ -1,3 +1,14 @@
+let navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if(window.scrollY >= 600){
+    navbar.classList.add('navbar_background');
+  }else{
+    navbar.classList.remove('navbar_background');
+  }
+});
+
+
 var typed = new Typed(".order", {
     strings: ["Order Your"],
     typeSpeed: 250,
